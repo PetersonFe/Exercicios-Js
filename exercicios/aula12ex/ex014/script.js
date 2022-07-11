4538c4b4511ddc50996064bca3f5ea78197b0c22
PetersonFe <Petersonf235@gmail.com>
2022-07-11 09:37:22 -0400

    Update

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -8,15 +8,15 @@ function carregar() {
     msg.innerHTML = `${hora}:${minuto}:${segundo}`
     if (hora >= 0 && hora <= 12) {
         //BOM DIA!
-        img.src= "img/manha.jpg"
+        img.src = "img/manha.jpg"
         document.body.style.background = '#4e2f04'
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE!
-        img.src= "img/tarde.jpg"
+        img.src = "img/tarde.jpg"
         document.body.style.background = '#7e8182'
     } else {
         // BOA NOITE!
-        img.src= "img/noite.jpg"
+        img.src = "img/noite.jpg"
         document.body.style.background = '#102a3f'
     }
 }
---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,18 @@
+
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/*
+var c = 1
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++
+};
+*/
