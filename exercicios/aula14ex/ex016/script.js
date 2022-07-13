f4a65ab246c734e09dbe80f3ff02534ea501411f
PetersonFe <Petersonf235@gmail.com>
2022-07-13 08:10:42 -0400

    13/07/2022

---
+++ exercicios/aula14ex/ex016/script.js
@@ -0,0 +1,15 @@
+function contarNumeros() {
+    var ini = window.document.getElementById("ini")
+    var fim = window.document.getElementById("fim")
+    var pas = document.getElementById("pas")
+    var res = document.getElementById("res")
+    var n1 = Number(ini.value)
+    var n2 = Number(fim.value)
+    var n3 = Number(pas.value)
+    msg.innerHTML = 'Ta fucionando'
+    if (txtn1 == '2' ) {
+        msg.innerHTML = 'Ta fucionando'
+    }
+
+
+}
