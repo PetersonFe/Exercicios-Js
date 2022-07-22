23e004421b380b14c992e99707d31979a4aa3096
PetersonFe <102931633+PetersonFe@users.noreply.github.com>
2022-07-22 14:54:38 -0400

    Update 22/07/2022

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,4 +1,4 @@
-function contarNumeros() {
+function contar() {
     var ini = window.document.getElementById("ini")
     var fim = window.document.getElementById("fim")
     var pas = document.getElementById("pas")
@@ -6,10 +6,20 @@ function contarNumeros() {
     var n1 = Number(ini.value)
     var n2 = Number(fim.value)
     var n3 = Number(pas.value)
-    msg.innerHTML = 'Ta fucionando'
-    if (txtn1 == '2' ) {
-        msg.innerHTML = 'Ta fucionando'
+
+    if (ini.value.length == 0 || Number(fim.value) <= Number(ini.value) || Number(ini.value) < 0) {
+        alert("[ERRO] Algo deu errado! Preencha o campo de forma correta!")
+        res.innerHTML = `Impossível Contar!`
+    } else if (fim.value.length == 0 || Number(fim.value) <= 0) {
+        alert("[ERRO] Algo deu errado! Preencha o campo de forma correta!")
+        res.innerHTML = "Impossível Contar!"
+    } else if (pas.value.length == 0 || Number(pas.value) <= 0) {
+        alert("[ERRO] PASSO INVÁLIDO! O valor será reajustado para 1!")
+        pas.value = 1
+        res.innerHTML = "Tente Novamente!"
+    } else {
+        for (var n1; n1 <= n2; n1 += n3) {
+            res.innerHTML += `${n1}`
+        }
     }
-
-
 }
